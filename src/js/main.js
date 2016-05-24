105cd2f2c0914a7c744abcdb799a084b2598ab38
Lucio Villa <lucio310@gmail.com>
2016-05-24 11:14:19 -0700

    replaced infobox video with gif

--- src/js/main.js
+++ src/js/main.js
@@ -251,3 +251,14 @@ $(function() {
 $(".anchor").click(function(event) {
   $('body').animate( { scrollTop:$(this.hash).offset().top} , 1000);
 } );
+
+
+var $elem = $('#infobox .item'), l = $elem.length, i = 0;
+function go() {
+    $elem.eq(i % l).fadeIn(4000, function() {
+        $elem.eq(i % l).fadeOut(4000, go);
+        i++;
+    })
+}
+
+go();
